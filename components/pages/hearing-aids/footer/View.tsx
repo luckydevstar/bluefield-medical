6aee7fc3e5137d98d042da0d63d3b075f3d21993
adryan.daniel20255@gmail.com <adryan.daniel20255@gmail.com>
2025-08-21 10:44:30 -0300

    fix: change footer from blue to white

--- components/pages/hearing-aids/footer/View.tsx
+++ components/pages/hearing-aids/footer/View.tsx
@@ -42,7 +42,7 @@ interface FooterViewProps {
 
 export function HearingAidsFooterView({ data }: FooterViewProps) {
     return (
-        <footer className="bg-darkblue">
+        <footer className="bg-white">
             <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
                 <div className='w-full grid grid-cols-3 gap-x-4 gap-y-12'>
                     <div className='col-span-3 md:col-span-1 lg:col-span-1 flex flex-col gap-6'>
@@ -54,24 +54,24 @@ export function HearingAidsFooterView({ data }: FooterViewProps) {
                                 data.social.map((item, idx) => {
                                     let SocialIcon = Icons[item.name]
                                     return (
-                                        <Link href={item.url} className='bg-lightblue hover:bg-white transition-colors rounded-full px-2 py-2 flex items-center justify-center' key={idx}>
+                                        <Link href={item.url} className='bg-darkblue hover:bg-darkblue transition-colors rounded-full px-2 py-2 flex items-center justify-center' key={idx}>
                                             {SocialIcon && <SocialIcon className="w-4 h-4" />}
                                         </Link>
                                     )
                                 })
                             }
                         </div>
-                        <p className='text-lightblue text-sm'>
+                        <p className='text-darkblue text-sm'>
                             {data.address}
                         </p>
                     </div>
                     <div className='flex flex-col gap-4 col-span-3 md:col-span-1 lg:col-span-1'>
-                        <h2 className='text-lg font-black text-lightblue'>RESOURCES</h2>
+                        <h2 className='text-lg font-black text-darkblue'>RESOURCES</h2>
                         <div className='flex flex-col gap-3'>
                             {
                                 data.resources.map((item, idx) => {
                                     return (
-                                        <Link href={item.url} key={idx} className='flex flex-col gap-1 text-md text-white'>
+                                        <Link href={item.url} key={idx} className='flex flex-col gap-1 text-md text-darkblue'>
                                             <span className='font-medium'>{item.name}</span>
                                         </Link>
                                     )
@@ -80,12 +80,12 @@ export function HearingAidsFooterView({ data }: FooterViewProps) {
                         </div>
                     </div>
                     <div className='flex flex-col gap-4 col-span-3 md:col-span-1 lg:col-span-1'>
-                        <h2 className='text-lg font-black text-lightblue'>COMPANY</h2>
+                        <h2 className='text-lg font-black text-darkblue'>COMPANY</h2>
                         <div className='flex flex-col gap-3'>
                             {
                                 data.company.map((item, idx) => {
                                     return (
-                                        <Link href={item.url} className='flex flex-col gap-1 text-md text-white' key={idx}>
+                                        <Link href={item.url} className='flex flex-col gap-1 text-md text-darkblue' key={idx}>
                                             <span className='font-medium'>{item.name}</span>
                                         </Link>
                                     )
