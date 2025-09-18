56d17b0bd63b56aea69a70fe4a1bef87d881d69d
adryan.daniel20255@gmail.com <adryan.daniel20255@gmail.com>
2025-09-18 01:27:20 -0300

    favicon chage

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,18 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Bluefield Medical',
   description: 'Medical Services',
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon.ico', rel: 'shortcut icon' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+    other: [
+      { rel: 'mask-icon', url: '/safari-pinned-tab.svg', color: '#0ea5e9' },
+      { rel: 'manifest', url: '/site.webmanifest' },
+    ],
+  },
 };
 
 export default function RootLayout({
